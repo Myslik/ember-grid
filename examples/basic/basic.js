a991e4534eb767c2175429f6c2d6438f3ec8e735
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2013-04-20 02:54:18 +0200

    Initial release

---
+++ examples/basic/basic.js
@@ -0,0 +1,19 @@
+﻿App.IndexRoute = Ember.Route.extend({
+    model: function () {
+        return App.Person.find();
+    },
+    setupController: function (controller, model) {
+        controller.set('content', model);
+    }
+});
+
+App.IndexController = GRID.TableController.extend({
+
+    columns: [
+        GRID.column('name'),
+        GRID.column('age')
+    ]
+
+});
+
+App.IndexView = GRID.TableView.extend();
